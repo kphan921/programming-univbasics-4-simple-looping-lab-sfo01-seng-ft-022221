1e1a8d7ad6beffc4c4e7e06d265f3a73bf5c0ad5
eager-patch-2604 <triphan921@gmail.com>
2021-01-30 02:54:58 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,8 +1,8 @@
 # Write your methods here
 
-def loop_message_five_times(message)
+def loop_message_five_times(message, limit)
   count = 0
-  while count < 5
+  while count < limit
     puts message
     count += 1
 end
