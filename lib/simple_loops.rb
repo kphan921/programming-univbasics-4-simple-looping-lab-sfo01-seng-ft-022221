44af7dca0f59bf43ce667423492ad73b2a1d6106
eager-patch-2604 <triphan921@gmail.com>
2021-01-30 02:49:00 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,15 +1,17 @@
 # Write your methods here
 
-def loop_message_five_times
+def loop_message_five_times(array)
   
 end
 
-def loop_message_n_times
+def loop_message_n_times(array)
   
 end
 
-def output_array
+def output_array(array)
   
 end
 
-def 
+def return_string_array(array)
+  
+end
